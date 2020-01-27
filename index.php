57bf71140883422558ebbd906f557f098adf93a6
Elijah Maret <Emaret2@mail.greenriver.edu>
2020-01-27 13:20:27 -0500

    added session variables

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 * Elijah Maret
 * date: Jan/24/2020
 * URL: http://ekaur.greenriverdev.com/328/pets2/index.php
- * URL: http://emaret.greenriverdev.com/328/pets2/index.php
+* URL: http://emaret.greenriverdev.com/328/pets2/index.php
 * description:
 */
 
 
 //start a session
-//session_start();
+session_start();
 
 // Turn on error reporting
 ini_set('display_errors', 1);
@@ -41,12 +41,13 @@ $f3->route('GET /order', function() {
 
 $f3->route('POST /order2', function() {
     //var_dump($_POST);
-    //$_SESSION['order'] = $_POST['order'];
+    $_SESSION['animal'] = $_POST['animal'];
     $view = new Template();
     echo $view->render('views/form2.html');
 });
 
 $f3->route('POST /results', function() {
+    $_SESSION['color'] = $_POST['color'];
     $view = new Template();
     echo $view->render('views/results.html');
 });
